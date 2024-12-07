e530d8dbe95162f1b9ef18497871c2a42023d196
guptadheerajj <guptadheeraj150104@gmail.com>
2024-12-07 13:09:26 +0530

    Completed Task10 of JavaScript assessment

---
+++ mentees/guptadheerajj/Assessment1/Javascript/Task10/task10.js
@@ -0,0 +1,19 @@
+// Q1.
+let sum = (num1, num2) => {
+    return num1 + num2;
+};
+
+console.log("Sum is: ", sum(2, 8));
+
+// Q2.
+let myJsonString = `{
+    "name": "Dheeraj",
+    "age": 20,
+    "interests": "Propgramming"
+}`;
+
+let userProfile = JSON.parse(myJson);
+
+console.log(userProfile["name"]);
+console.log(userProfile["age"]);
+console.log(userProfile["interests"]);
